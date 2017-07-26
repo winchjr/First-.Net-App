cc1a59ee1f64b189f0477cee589bbb3e48d34f81
JW <winchesterjoshuar@gmail.com>
2017-07-26 08:41:12 -0400

    Second push

---
+++ WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult UserRegistration()
+        {
+            ViewBag.Message = "Please enter the following information to sign up.";
+            return View();
+        }
+
+        public ActionResult Summary(Models.Register r)
+        {
+            ViewBag.Message = "Your name is " + r.FirstName;
+            ViewBag.Message += " ";
+
+            ViewBag.Message += "Your email is " + r.Email;
+            ViewBag.Message += " ";
+            ViewBag.Message += "Your gender is" + r.Gender;
+        return View(r);
+        }
+
+    }
+}
---
+++ WebApplication3/Models/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Web.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Remoting.Contexts;
+using Microsoft.Ajax.Utilities;
+using System.Reflection.Emit;
+
+namespace WebApplication3.Models
+
+
+{
+
+    public class Register
+
+    {
+
+        [Required]
+
+        [Key]
+
+        public string FirstName { get; set; }
+
+        [Required]
+
+        public string Email { get; set; }
+
+        public string Gender { get; set; }
+
+        }
+
+    }
